138907bacf7eafd222392e90c7a5d480cba3ba90
Gabriel Bucher <gabriel@humblecoders.com>
2023-06-01 14:15:30 +0800

    add openai

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,28 @@
-import { Typography, TextField, Button, Stack } from "@mui/material";
+import { Typography, TextField, Button, Stack, Card, CardContent } from "@mui/material";
+import { useRef, useCallback, useState } from "react";
 
 export default function Home() {
-    function generateBlurb(): void {
-        throw new Error("Function not implemented.");
-    }
+    const blurbRef = useRef("");
+    const [generatingPosts, setGeneratingPosts] = useState("");
+
+    const generateBlurb = useCallback(async () => {
+        const response = await fetch("/api/generateBlurb", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                prompt: blurbRef.current,
+            }),
+        });
+        if (!response.ok) {
+            throw new Error(response.statusText);
+        }
+        const data = await response.json();
+        console.log("Response was:", JSON.stringify(data));
+        setGeneratingPosts(data.choices[0].message.content);
+    }, [blurbRef.current]);
+
     return (
         <Stack
             component="main"
@@ -26,11 +45,19 @@ export default function Home() {
                 multiline
                 fullWidth
                 minRows={4}
+                onChange={(e) => {
+                    blurbRef.current = e.target.value
+                }}
                 sx={{ "& textarea": { boxShadow: "none !important" } }}
                 placeholder="Key words on what you would like your blurb to be about"
             ></TextField>
 
             <Button onClick={generateBlurb}>Generate Blurb</Button>
+            {generatingPosts && (
+                <Card>
+                    <CardContent>{generatingPosts}</CardContent>
+                </Card>
+            )}
         </Stack>
     );
 }
